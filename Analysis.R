34d1801e9bfb8ac7248ff4f25f94e01d0438a979
Gerard Gomez Jorba <58191927+GerardGJ@users.noreply.github.com>
2022-10-11 13:05:10 +0200

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -1,8 +1,11 @@
 library(naniar)
 library(impute)
+library(stringi)
+library(class)
 
 setwd("/Users/Gerard/Desktop/MVA/Project/")
 USAccidents <- read.csv("final_US_Accidents.csv")
+USAccidents <- USAccidents[,2:21]
 str(USAccidents)
 
 
@@ -42,11 +45,12 @@ USAccidents$Weather_Condition <- as.factor(USAccidents$Weather_Condition)
 USAccidents$Year <- as.factor(USAccidents$Year)
 USAccidents$County <- as.factor(USAccidents$County)
 USAccidents$Severity <- as.factor(USAccidents$Severity)
+USAccidents$Humidity... <- as.numeric(USAccidents$Humidity...)
 
 levels(USAccidents$Weather_Condition)
 
 #Treating NA####
-little_result <- mcar_test(USAccidents[,2:21]) #p-val = 0 55 different patterns 
+little_result <- mcar_test(USAccidents) #p-val = 0 55 different patterns 
 
 summary(USAccidents) #First look at which variable have the most na and try to do a logical imputation
 
@@ -73,7 +77,36 @@ for(good  in goodWeather){
   USAccidents$Wind_Chill.F.[which(is.na(USAccidents$Wind_Chill.F.) & USAccidents$Weather_Condition == good)] <- meanwind
 }
 
-little_result <- mcar_test(USAccidents[,2:21]) #p-val = 0, 54 different patterns
+#City: We impute the values of the city with the county capitals
+USAccidents$City <- as.character(USAccidents$City)
+USAccidents[is.na(USAccidents$City),] #Los Angeles, Santa Barbara, Hamilton, Placer, Siskiyou
+USAccidents$City[50666] <- "Los Angeles"
+USAccidents$City[519148] <- "Santa Barbara"
+USAccidents$City[557014] <- "Santa Barbara"
+USAccidents$City[596759] <- "Santa Barbara"
+USAccidents$City[598693] <- "Los Angeles"
+USAccidents$City[799137] <- "Chattanooga"
+USAccidents$City[954736] <- "Auburn"
+USAccidents$City[988180] <- "Yreka"
+USAccidents$City <- as.factor(USAccidents$City)
+
+
+USAccidents$Month <- as.numeric(substr(USAccidents$Start_Time,6,7))
+USAccidents$Season <- NA
+USAccidents$Season[USAccidents$Month == 12 | USAccidents$Month == 1 | USAccidents$Month == 2] = "Winter"
+USAccidents$Season[USAccidents$Month == 6 | USAccidents$Month == 7 | USAccidents$Month == 8] = "Summer"
+USAccidents$Season[USAccidents$Month == 9 | USAccidents$Month == 10 | USAccidents$Month == 11] = "Autumn"
+USAccidents$Season[USAccidents$Month == 3 | USAccidents$Month == 4 | USAccidents$Month == 5] = "Spring"
+
+#As temperature is normal will impute its values randomly from a normal distribution with the same mean an st dev:
+qqnorm(USAccidents$Temperature.F., pch = 1, frame = FALSE)
+qqline(USAccidents$Temperature.F., col = "steelblue", lwd = 2)  
+
+set.seed(123)
+sum(is.na(USAccidents$Temperature.F.))
+USAccidents$Temperature.F.[is.na(USAccidents$Temperature.F.)] <- rnorm(sum(is.na(USAccidents$Temperature.F.)),mean = mean(USAccidents$Temperature.F.,na.rm = T), sd = sd(USAccidents$Temperature.F.,na.rm = T))
+
+
 
 
 #Look at how many missing rows has each accident
@@ -82,3 +115,7 @@ na_count <- apply(USAccidents, 1, function(x) sum(is.na(x)))
 table(na_count)
 
 sum(table(na_count)[2:8])/1067434 #50606/1067434 = 0.04740902 
+
+
+
+USAccidents[is.na(USAccidents$City),]
