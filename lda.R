b8e5c505203118c94996e4de1fbda7ac1c133f0d
Odysseas <odykypar@gmail.com>
2022-12-16 18:34:00 +0100

    LDA final version

--- lda.R
+++ lda.R
@@ -1,39 +1,24 @@
-install.packages("MASS")
+# Importing Libraries
 library(MASS)
-# https://little-book-of-r-for-multivariate-analysis.readthedocs.io/en/latest/src/multivariateanalysis.html
-setwd('/Users/ander.barrio/Desktop/MVA-Project')
-df <- read.csv("US_Accidents_DT.csv", sep=",",dec=".",stringsAsFactors = TRUE)
-names(df)
-dim(df)
-summary(df)
+library(car)
+library(stringr)
+library(caret)
+library(pROC)
+
+# Setting-up Working Environment
+working_dir = "C:/Users/odyky/Desktop/UPC/1st Semester/MVA-MDS/Group project/MVA-Project-Code/USAccidents"
+plot_dir = "C:/Users/odyky/Desktop/UPC/1st Semester/MVA-MDS/Group project/MVA-Project-Code/Plots/DiscriminantAnalysis"
+dir.create(plot_dir, showWarnings = FALSE) # Creating the folder for saving the plots of Discriminant Analysis
+setwd(working_dir)
+
+# Loading Data and Basic Summary
+df <- read.csv("../US_Accidents_DT.csv", sep=",",dec=".",stringsAsFactors = TRUE)
 df <- df[-c(1)]
+dim(df)
+names(df)
 summary(df)
 
-
-
-df.lda <- lda(df$Severity ~ df$Visibility.mi. + df$Precipitation.in. + df$Humidity... + 
-                df$Temperature.F. + df$Distance.mi. + df$Pressure.in.)
-
-df.lda
-
-#coeficients de la funcio discriminant
-df.lda$scaling[,1]
-df.lda$scaling[,2]
-df.lda$scaling[,3]
-
-
-df.lda$scaling[,1:3]
-
-#valors de cada cas per la primera funcio discriminant
-df.lda.values <- predict(df.lda, df[,c(6:11)])
-
-df[,20]<- df.lda.values$x[,1]
-df[,21]<- df.lda.values$x[,2]
-df[,22]<- df.lda.values$x[,3]
-names(df)[20]<-"LDA1"
-names(df)[21]<-"LDA2"
-names(df)[22]<-"LDA3"
-
+# Defining Functions
 calcWithinGroupsVariance <- function(variable,groupvariable)
 {
   # find out how many values the group variable can take
@@ -60,7 +45,6 @@ calcWithinGroupsVariance <- function(variable,groupvariable)
   return(Vw)
 }
 
-
 groupStandardise <- function(variables, groupvariable)
 {
   # find out how many variables we have
@@ -82,17 +66,6 @@ groupStandardise <- function(variables, groupvariable)
   }
   return(variables_new)
 }
-#si s'estandarditzen les variables es solen obtenir valors mes interpretables
-dfStandard <- groupStandardise(df[,c(6:11)], df[13])
-
-dfStandard.lda <- lda(df$Severity ~ dfStandard$Visibility.mi. + dfStandard$Precipitation.in. + dfStandard$Humidity... + 
-                        dfStandard$Temperature.F. + dfStandard$Distance.mi. + dfStandard$Pressure.in.)
-
-dfStandard.lda
-
-df.lda.values2 <- predict(dfStandard.lda, dfStandard)
-df.lda.values
-df.lda.values2
 
 calcBetweenGroupsVariance <- function(variable,groupvariable)
 {
@@ -124,7 +97,6 @@ calcBetweenGroupsVariance <- function(variable,groupvariable)
   return(Vb)
 }
 
-
 calcSeparations <- function(variables,groupvariable)
 {
   # find out how many variables we have
@@ -143,99 +115,6 @@ calcSeparations <- function(variables,groupvariable)
     print(paste("variable",variablename,"Vw=",Vw,"Vb=",Vb,"separation=",sep))
   }
 }
-
-#separacio que donen les dues funcions discriminants (ratio de variancia
-#entre respecte v intra)
-calcSeparations(df.lda.values$x,df[13])
-calcSeparations(df.lda.values2$x,df[13])
-(df.lda$svd)^2
-
-#total separation (la suma de les dues)
-#percentatge que separa cada una, coincideix amb la proportion of trace del model discriminant
-
-df.lda
-
-hist(df.lda.values$x[,1])
-hist(df.lda.values$x[,2])
-hist(df.lda.values$x[,3])
-#histograma multiple entre la funcio discriminant i la resposta
-par("mar")
-par(mar=c(1,1,1,1))
-par(mar=c(5.1,4.1,4.1,2.1))
-par(mar=c(3,2.5,1.5,1))
-
-
-ldahist(data = df.lda.values$x[,1], g=df$Severity, ymax=1)
-
-ldahist(data = df.lda.values$x[,2], g=df$Severity, ymax = 1)
-
-ldahist(data = df.lda.values$x[,3], g=df$Severity, ymax = 1)
-
-#plot de les dues components discriminants (etiquetem els grups)
-plot(df.lda.values$x[,1],df.lda.values$x[,2]) # make a scatterplot
-text(df.lda.values$x[,1],df.lda.values$x[,2],df$Severity,cex=0.7,pos=4,col="red") # add labels
-
-
-df.lda$scaling[,1]
-df.lda$scaling[,2]
-df.lda$scaling[,3]
-
-
-n<-dim(df)[1]
-for(i in 1:n){
-  if(wine[i,15]<0){wine[i,17]<-2
-  }else{if (wine[i,16]<0){wine[i,17]<-1 
-  }else{
-    wine[i,17]<-3
-  }
-  }
-}
-
-
-#matriu de confusio
-table(wine[,1])
-MC<-table(wine[,1], wine[,17])
-MC
-
-
-#accuracy
-accuracy<-sum(diag(MC))/dim(wine)[1]
-accuracy
-
-#compute missclassification rate
-MR<-1-accuracy
-MR
-
-#buscar punts intermedis de les mitjanes i utilitzarlos per definir les regles de classificacio
-
-
-printMeanAndSdByGroup <- function(variables,groupvariable)
-{
-  # find the names of the variables
-  variablenames <- c(names(groupvariable),names(as.data.frame(variables)))
-  # within each group, find the mean of each variable
-  groupvariable <- groupvariable[,1] # ensures groupvariable is not a list
-  means <- aggregate(as.matrix(variables) ~ groupvariable, FUN = mean)
-  names(means) <- variablenames
-  print(paste("Means:"))
-  print(means)
-  # within each group, find the standard deviation of each variable:
-  sds <- aggregate(as.matrix(variables) ~ groupvariable, FUN = sd)
-  names(sds) <- variablenames
-  print(paste("Standard deviations:"))
-  print(sds)
-  # within each group, find the number of samples:
-  samplesizes <- aggregate(as.matrix(variables) ~ groupvariable, FUN = length)
-  names(samplesizes) <- variablenames
-  print(paste("Sample sizes:"))
-  print(samplesizes)
-}
-
-#mitjanes de les funcions discriminants per grups
-printMeanAndSdByGroup(df.lda.values$x,df[13])
-c1 <- (0.2165391 - 0.3092213) / 2
-c2 <- (- 0.3092213 + 0.5002339) / 2
-c3 <- (0.5002339 + 0.1311213) / 2
 
 calcAllocationRuleAccuracy <- function(ldavalue, groupvariable, cutoffpoints)
 {
@@ -279,4 +158,185 @@ calcAllocationRuleAccuracy <- function(ldavalue, groupvariable, cutoffpoints)
   }
 }
 
-calcAllocationRuleAccuracy(df.lda.values$x[,1], df[13], c(c1, c2, c3))
+printMeanAndSdByGroup <- function(variables,groupvariable)
+{
+  # find the names of the variables
+  variablenames <- c(names(groupvariable),names(as.data.frame(variables)))
+  # within each group, find the mean of each variable
+  groupvariable <- groupvariable[,1] # ensures groupvariable is not a list
+  means <- aggregate(as.matrix(variables) ~ groupvariable, FUN = mean)
+  names(means) <- variablenames
+  print(paste("Means:"))
+  print(means)
+  # within each group, find the standard deviation of each variable:
+  sds <- aggregate(as.matrix(variables) ~ groupvariable, FUN = sd)
+  names(sds) <- variablenames
+  print(paste("Standard deviations:"))
+  print(sds)
+  # within each group, find the number of samples:
+  samplesizes <- aggregate(as.matrix(variables) ~ groupvariable, FUN = length)
+  names(samplesizes) <- variablenames
+  print(paste("Sample sizes:"))
+  print(samplesizes)
+}
+
+# Applying Linear Discriminant Analysis
+
+# Splitting data to training and test sets.
+# 1/3 will be used for training and 2/3 for testing the model.
+set.seed(123)
+NUMBERROWS <- nrow(df)
+learn <- sample(1:NUMBERROWS, round(0.67*NUMBERROWS))
+
+training_full <- df[learn,]
+test_full = df[-learn,]
+training <-cbind(training_full[,c(6:11)], training_full[13])
+test <-cbind(test_full[,c(6:11)], test_full[13])
+
+
+# TRAINING STEP
+
+# Using only the numerical values of the data set.
+# If the variables are standardized, the results obtained
+# are usually easier to interpret.
+training.Standard <- groupStandardise(training[,c(1:6)], training[7])
+training.Standard$Severity <- training$Severity
+test.Standard <- groupStandardise(test[,c(1:6)], test[7])
+test.Standard$Severity <- test$Severity
+
+
+summary(training.Standard)
+summary(test.Standard)
+
+training.lda <- lda(Severity ~., data = training.Standard)
+
+training.lda
+
+
+# Coefficients of the Discriminant Functions
+training.lda$scaling[,1:3]
+
+# Values of each case for the first discriminant function
+training.lda.values <- predict(training.lda, training.Standard[,c(1:6)])
+
+LDA1<- training.lda.values$x[,1]
+LDA2<- training.lda.values$x[,2]
+LDA3<- training.lda.values$x[,3]
+
+# Separation given by the discriminant functions.
+calcSeparations(training.lda.values$x,training.Standard$Severity)
+# Total Separation -> 752.2910036642373
+(training.lda$svd)^2 # This command returns the separation of the groups
+# Same results with calcSeparations function
+
+
+# Stacked Histograms of the LDA Values
+ldahist(data = LDA1, g=training.Standard$Severity, main="Stacked Histogram of the 1st Discriminant Function’s Values")
+
+ldahist(data = LDA2, g=training.Standard$Severity, main="Stacked Histogram of the 2nd Discriminant Function’s Values")
+
+ldahist(data = LDA3, g=training.Standard$Severity, main="Stacked Histogram of the 3rd Discriminant Function’s Values")
+
+#plot  of LDA axis and observations
+SeverityNum = str_sub(training.Standard$Severity,-1)
+plot(LDA1,LDA2, main="LDA1 and LDA2 Projection of Training Data",
+     xlab="LDA1", ylab="LDA2")
+text(LDA1,LDA2, SeverityNum, cex=0.5, pos=4, col=SeverityNum)
+
+# We can glue together the posterior-probabilities and the predicted class
+# vector
+training_posterior <- training.lda.values$posterior
+training_pred <- training.lda.values$class
+head(cbind(round(training_posterior, 4), class=training_class),10)
+
+# plot(training.lda, col=as.numeric(SeverityNum)) # assign color code based on factor code --> APPENDIX
+
+# Confusion matrix (training error)
+
+training_conf_with_lib <- confusionMatrix(data=training_pred, reference = training.Standard$Severity, mode="everything")
+training_conf_with_lib
+training_conf <- table(list(predicted=training_pred, observed=training.Standard$Severity))
+training_conf
+
+# Training Accuracy
+training_accuracy<-sum(diag(training_conf))/dim(training.Standard)[1]; training_accuracy
+
+# Training Precision (Positive predicted value)
+training_precision <- diag(training_conf) / rowSums(training_conf); training_precision
+
+# Compute Training Missclassification Rate
+training_mr <- 1-training_accuracy; training_mr
+
+# ROC for Training Set
+train_prob = predict(training.lda, training.Standard[1:6], type = "response")
+
+resut = multiclass.roc(training.Standard$Severity, train_prob$posterior)
+
+pdf(paste(plot_dir, "AUC_train", ".pdf", sep=""))
+par(mfrow=c(2,3))
+for (contrast in names(result$rocs)) {
+  plot(result$rocs[[contrast]][[1]], col = "#006666", main = contrast)
+  plot(result$rocs[[contrast]][[2]], col = "#666666", add = TRUE)
+  line1<-str_split(contrast,"/",simplify = TRUE)[1]
+  line2<-str_split(contrast,"/",simplify = TRUE)[2]
+  auc1<-auc(result$rocs[[contrast]][[1]])
+  auc1<- round(auc1, digits = 3)
+  auc2<-auc(result$rocs[[contrast]][[2]])
+  auc2<- round(auc2, digits = 3)
+  text<- " with AUC: "
+  legend(x="topleft",legend=c(paste(line1,text, as.character(auc1)),paste(line2,text, as.character(auc2))),
+         col=c("#006666", "#666666"), lwd=5, cex=0.8)
+  
+}
+dev.off()
+par(mfrow=c(1,1))
+
+# TESTING STEP
+
+# Values of each case for the first discriminant function
+test.lda.values <- predict(training.lda, newdata = test.Standard[1:6])
+test_predictions <- test.lda.values$class
+
+# We can glue together the posterior-probabilities and the predicted class
+# vector
+test_posterior <- test.lda.values$posterior
+head(cbind(round(test_posterior, 4), class=test_predictions),10)
+
+# Confusion matrix (test error)
+
+test_conf_with_lib <- confusionMatrix(data=test_predictions, reference = test.Standard$Severity, mode="everything")
+test_conf_with_lib
+test_conf <- table(list(predicted=test_predictions, observed=test.Standard$Severity))
+test_conf
+
+# test Accuracy
+test_accuracy<-sum(diag(test_conf))/dim(test.Standard)[1]; test_accuracy
+
+# test Precision (Positive predicted value)
+test_precision <- diag(test_conf) / rowSums(test_conf); test_precision
+
+# Compute test Missclassification Rate
+test_mr <- 1-test_accuracy; test_mr
+
+# ROC for test Set
+test_prob = predict(training.lda, test.Standard[1:6], type = "response")
+result = multiclass.roc(test.Standard$Severity,test_prob$posterior)
+
+pdf(paste(plot_dir, "AUC_test", ".pdf", sep=""))
+par(mfrow=c(2,3))
+for (contrast in names(result$rocs)) {
+  plot(result$rocs[[contrast]][[1]], col = "#006666", main = contrast)
+  plot(result$rocs[[contrast]][[2]], col = "#666666", add = TRUE)
+  line1<-str_split(contrast,"/",simplify = TRUE)[1]
+  line2<-str_split(contrast,"/",simplify = TRUE)[2]
+  auc1<-auc(result$rocs[[contrast]][[1]])
+  auc1<- round(auc1, digits = 3)
+  auc2<-auc(result$rocs[[contrast]][[2]])
+  auc2<- round(auc2, digits = 3)
+  text<- " with AUC: "
+  legend(x="topleft",legend=c(paste(line1,text, as.character(auc1)),paste(line2,text, as.character(auc2))),
+         col=c("#006666", "#666666"), lwd=5, cex=0.8)
+  
+}
+dev.off()
+par(mfrow=c(1,1))
